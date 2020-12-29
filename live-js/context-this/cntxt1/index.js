b535e81748e7ce657eafa763696cfa42ab5b773f
ki4a <Igor.ki4@gmail.com>
2020-12-29 16:07:31 +0200

    23-3

--- live-js/context-this/cntxt1/index.js
+++ live-js/context-this/cntxt1/index.js
@@ -1,4 +1,4 @@
-
+/*
 const timer = {
      secondsPassed: 0,
      minsPassed: 0,
@@ -8,6 +8,7 @@ const timer = {
                this.secondsPassed +=1
                if(this.secondsPassed > 59) {
                     this.secondsPassed = 0;
+                    this.minsPassed +=1
                }
           }, 1000);
           setInterval(() => {
@@ -37,6 +38,47 @@ const timer = {
      },
 
 }
-
+*/
 
 /*-------------------------------------------------------*/
+// Fix context - bind, () =>.
+// SetInterval
+// In: function + interval (ms)
+// Out: Setinterval ID functionResult ID
+
+const timer = {
+  secondsPassed: 0,
+  minsPassed: 0,
+  timerId: null,
+
+  /*-------*/
+  startTimer() {
+    // in: func, num
+    // out: num
+     this.timerId = setInterval(() => {
+      this.secondsPassed += 1;
+      if (this.secondsPassed === 60) {
+        this.secondsPassed = 0;
+        this.minsPassed += 1;
+      }
+    }, 1000);
+  },
+
+  /*-------*/
+  stopTimer() {
+    clearInterval(this.timerId);
+  },
+
+  getTime() {
+     const seconds = this.secondsPassed;
+     const mins = this.minsPassed;
+     return `${mins}:0${seconds}`
+  },
+
+  resetTimer() {
+     this.secondsPassed = 0;
+     this.minsPassed = 0;
+  },
+};
+
+timer.startTimer();
---
+++ online.js/lesson23-events-forms-delegation/task3/index.js
@@ -0,0 +1,65 @@
+const listElem = document.querySelector('.list');
+
+const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
+
+const renderTasks = tasksList => {
+  const tasksElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.checked = done;
+      checkbox.classList.add('list__item-checkbox');
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+      listItemElem.append(checkbox, text);
+
+      return listItemElem;
+    });
+
+  listElem.append(...tasksElems);
+};
+renderTasks(tasks);
+
+/*----------------*/
+
+const taskChanger = event => {
+  if (!event.target.classList.contains('list__item-checkbox')) {
+    return;
+  }
+  const checkedLi = event.target.closest('.list__item');
+  const checkedBox = tasks.find(obj => obj.id === checkedLi.dataset.id);
+  checkedBox.done = event.target.checked;
+  listElem.innerHTML = '';
+  renderTasks(tasks);
+};
+
+listElem.addEventListener('click', taskChanger);
+
+/*----------------*/
+
+const newTaskBtn = document.querySelector('.create-task-btn');
+const newTaskInField = document.querySelector('.task-input');
+
+const taskAdder = () => {
+  if (!newTaskInField.value) {
+    return;
+  }
+  tasks.unshift({
+    text: newTaskInField.value,
+    done: false,
+  });
+  newTaskInField.value = '';
+  listElem.innerHTML = '';
+  renderTasks(tasks);
+};
+newTaskBtn.addEventListener('click', taskAdder);
